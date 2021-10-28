078801c0890ada5a5c46a49f3bf11fb69eb7f87b
Ashley Davis <ashley@codecapers.com.au>
2021-10-28 13:45:47 +1000

    Fixed failing example test.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
-import * as moment from "moment";
-
-export class ExampleClass {
-
-    public test(): void {
-        console.log("Test: " + moment().format("YYYY-DD-MM"));
-    }
-
-    public returnsTrue(): boolean {
-        return true;
-    }
-
-}
 
 //TODO: Code for your reusable code module goes here.
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,10 +1,9 @@
-import { ExampleClass } from "../index";
 
 describe("main test suite", () => {
 
     it("should be true", ()  => {
 
-        expect((new ExampleClass()).returnsTrue()).toBeTruthy();
+        expect(true).toBeTruthy();
 
     });
 
