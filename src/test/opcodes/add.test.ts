1c4778d62f0fde6497ade21baf2523892d8e3b2f
Ashley Davis <ashley@codecapers.com.au>
2021-11-01 16:55:54 +1000

    Added separate tests for the add opcode.

---
+++ src/test/opcodes/add.test.ts
@@ -0,0 +1,23 @@
+import { Add } from "../../lib/opcodes/add";
+
+describe("add opcode", () => {
+
+    it ("can execute", () => {
+
+        const token = {
+            opcode: "+",
+            operands: [],
+        };
+        const context: any = {
+            stack: [
+                BigInt(3), 
+                BigInt(4),
+            ],
+        };
+        const opcode = new Add(token);
+        opcode.execute(context);
+
+        expect(context.stack.length).toEqual(1);
+        expect(Number(context.stack[0])).toEqual(7);
+    });
+});
