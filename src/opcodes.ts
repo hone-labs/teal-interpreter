70fcde5eb0cb075864e99146c0c32a0151f5eca3
Ashley Davis <ashley@codecapers.com.au>
2021-11-01 13:12:00 +1000

    Added types for opcode handler functions.

--- src/opcodes.ts
+++ src/opcodes.ts
@@ -1,21 +1,41 @@
 import { IExecuteResult, IInstruction } from ".";
+//
+// Represents validation and execution for an object.
+//
+export interface IOpcode {
 
-export const opcodeHandlers: any = {
+    //
+    // Executes the opcode.
+    //
+    execute(context: IExecuteResult, instruction: IInstruction): void;
+}
+
+//
+// A look up table for opcode handlers.
+//
+export interface IOpcodeMap {
+    [index: string]: IOpcode;
+}
+
+//
+// A look up table for opcode handlers.
+//
+export const opcodeHandlers: IOpcodeMap = {
     "int": {
-        execute: (context: IExecuteResult, instruction: IInstruction): void => {
+        execute: (context, instruction) => {
             const value = parseInt(instruction.operands[0]);
             context.stack.push(BigInt(value));
         },
     },
 
     "pop": {
-        execute: (context: IExecuteResult, instruction: IInstruction): void => {
+        execute: (context, instruction) => {
             context.stack.pop();
         },
     },
 
     "+": {
-        execute: (context: IExecuteResult, instruction: IInstruction): void => {
+        execute: (context, instruction): void => {
             const a = context.stack.pop() as bigint;
             const b = context.stack.pop() as bigint;
             context.stack.push(a + b);
