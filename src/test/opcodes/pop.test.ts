de0795d80fe7443484ba841990923d012ffd4ce1
Ashley Davis <ashley@codecapers.com.au>
2021-11-01 17:02:57 +1000

    Added separate tests for the pop opcode.

---
+++ src/test/opcodes/pop.test.ts
@@ -0,0 +1,21 @@
+import { Pop } from "../../lib/opcodes/pop";
+
+describe("pop opcode", () => {
+
+    it ("can execute", () => {
+
+        const token = {
+            opcode: "pop",
+            operands: [],
+        };
+        const context: any = {
+            stack: [
+                BigInt(3), 
+            ],
+        };
+        const opcode = new Pop(token);
+        opcode.execute(context);
+
+        expect(context.stack.length).toEqual(0);
+    });
+});
