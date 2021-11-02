958996833022fc32ba453edf89c1ba3a70c23331
Ashley Davis <ashley@codecapers.com.au>
2021-11-02 15:49:15 +1000

    Implemented tests for the branch opcode.

---
+++ src/test/opcodes/branch.test.ts
@@ -0,0 +1,42 @@
+import { Branch } from "../../lib/opcodes/branch";
+
+describe("branch opcode", () => {
+
+    it("throws when branch target doesn't exist", () => {
+
+        const token: any = {
+            opcode: "b",
+            operands: [
+                "a-label",
+            ],
+        };
+        const opcode = new Branch(token)
+
+        const context: any = {
+            branchTargets: {
+                // No branch target.
+            },
+        };
+        opcode.validateOperand();
+        expect(() => opcode.validateContext(context)).toThrow();
+    });
+
+    it ("can execute", () => {
+
+        const token: any = {
+            opcode: "b",
+            operands: [
+                "a-label",
+            ],
+        };
+        const opcode = new Branch(token);
+
+        const context: any = {
+            branchTargets: {
+                "a-label": 32,
+            },
+        };
+        opcode.validateOperand();
+        expect(opcode.execute(context)).toEqual(32);
+    });
+});
