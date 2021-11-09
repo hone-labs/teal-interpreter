5b4b041961bf31be2a58c159a7eeae5cce22a27b
Ashley Davis <ashley@codecapers.com.au>
2021-11-09 15:50:03 +1000

    Implemented equals operator for byte arrays (in addition to bigints).

--- src/test/opcodes/eq.test.ts
+++ src/test/opcodes/eq.test.ts
@@ -1,9 +1,29 @@
-import { makeBigInt } from "../../lib/context";
+import { makeBigInt, makeBytes } from "../../lib/context";
 import { Eq } from "../../lib/opcodes/eq";
 
 describe("eq opcode", () => {
 
-    it ("can execute", () => {
+    it ("comparing different types results in zero", () => {
+
+        const token: any = {
+            opcode: "==",
+            operands: [],            
+        };
+        const context: any = {
+            stack: [
+                makeBigInt(BigInt(3)),
+                makeBytes(new Uint8Array([1, 2, 3, 4])),
+            ],
+        };
+        const opcode = new Eq(token);
+        opcode.validateContext(context);
+        opcode.execute(context);
+
+        expect(context.stack.length).toEqual(1);
+        expect(Number(context.stack[0]?.value)).toEqual(0);
+    });
+
+    it ("can compare bigints that are different", () => {
 
         const token: any = {
             opcode: "==",
@@ -22,4 +42,64 @@ describe("eq opcode", () => {
         expect(context.stack.length).toEqual(1);
         expect(Number(context.stack[0]?.value)).toEqual(0);
     });
+
+    it ("can compare bigints that are equal", () => {
+
+        const token: any = {
+            opcode: "==",
+            operands: [],            
+        };
+        const context: any = {
+            stack: [
+                makeBigInt(BigInt(5)),
+                makeBigInt(BigInt(5)),
+            ],
+        };
+        const opcode = new Eq(token);
+        opcode.validateContext(context);
+        opcode.execute(context);
+
+        expect(context.stack.length).toEqual(1);
+        expect(Number(context.stack[0]?.value)).toEqual(1);
+    });
+
+    it ("can compare byte arrays that are different", () => {
+
+        const token: any = {
+            opcode: "==",
+            operands: [],            
+        };
+        const context: any = {
+            stack: [
+                makeBytes(new Uint8Array([1, 2, 3, 4])),
+                makeBytes(new Uint8Array([5, 6, 7, 8])),                
+            ],
+        };
+        const opcode = new Eq(token);
+        opcode.validateContext(context);
+        opcode.execute(context);
+
+        expect(context.stack.length).toEqual(1);
+        expect(Number(context.stack[0]?.value)).toEqual(0);
+    });    
+
+    it ("can compare byte arrays that are equal", () => {
+
+        const token: any = {
+            opcode: "==",
+            operands: [],            
+        };
+        const context: any = {
+            stack: [
+                makeBytes(new Uint8Array([1, 2, 3, 4])),
+                makeBytes(new Uint8Array([1, 2, 3, 4])),                
+            ],
+        };
+        const opcode = new Eq(token);
+        opcode.validateContext(context);
+        opcode.execute(context);
+
+        expect(context.stack.length).toEqual(1);
+        expect(Number(context.stack[0]?.value)).toEqual(1);
+    });        
 });
