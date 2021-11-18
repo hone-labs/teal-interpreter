dd55b14704b142477a71e7d66126c09c631bdd6b
Ashley Davis <ashley@codecapers.com.au>
2021-11-18 16:46:05 +1000

    Add missing export.

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { IOpcode } from "./lib/opcode";
 export { execute  } from "./lib/execute";
 export { IExecuteResult } from "./lib/execute-result";
 export { ValueType } from "./lib/context";
+export { ITealInterpreterConfig } from "./lib/config";
 export { ITealInterpreter, TealInterpreter } from "./lib/interpreter";
 
