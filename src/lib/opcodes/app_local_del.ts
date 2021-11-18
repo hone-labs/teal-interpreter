131f52e8d82c43a45a19719c61d16dd06a903ba2
Ashley Davis <ashley@codecapers.com.au>
2021-11-18 12:37:01 +1000

    Fixed account addr encoding in app_local_del opcode.

--- src/lib/opcodes/app_local_del.ts
+++ src/lib/opcodes/app_local_del.ts
@@ -1,12 +1,12 @@
 import { Opcode } from "../opcode";
 import { IExecutionContext } from "../context";
+import { encodeAddress } from "algosdk";
 
 export class AppLocalDel extends Opcode {
 
     execute(context: IExecutionContext): void {
         const localName = Buffer.from(this.popBytes(context)).toString();
-        const addr = this.popBytes(context);
-        const accountName = Buffer.from(addr).toString();
+        const accountName = encodeAddress(this.popBytes(context));
         const account = context.accounts[accountName];
 
         if (account === undefined) {
--- src/test/opcodes/app_local_del.test.ts
+++ src/test/opcodes/app_local_del.test.ts
@@ -1,4 +1,5 @@
 import { makeBigInt, makeBytes } from "../../lib/context";
+import { addressToBytes } from "../../lib/convert";
 import { opcodeDefs } from "../../lib/opcodes";
 import { AppLocalDel } from "../../lib/opcodes/app_local_del";
 
@@ -11,21 +12,21 @@ describe("app_local_del opcode", () => {
 
         const context: any = {
             accounts: {
-                AAAA: {
+                "7JOPVEP3ABJUW5YZ5WFIONLPWTZ5MYX5HFK4K7JLGSIAG7RRB42MNLQ224": {
                     locals: {
                         aLocal: makeBigInt(BigInt(2)),
                     },
                 },
             },            
             stack: [                
-                makeBytes(new Uint8Array(Buffer.from("AAAA"))),
+                makeBytes(addressToBytes("7JOPVEP3ABJUW5YZ5WFIONLPWTZ5MYX5HFK4K7JLGSIAG7RRB42MNLQ224")),
                 makeBytes(new Uint8Array(Buffer.from("aLocal"))),
             ],
         };
         opcode.execute(context);
 
         expect(context.stack.length).toEqual(0);
-        expect(context.accounts.AAAA.locals.aLocal).not.toBeDefined();
+        expect(context.accounts["7JOPVEP3ABJUW5YZ5WFIONLPWTZ5MYX5HFK4K7JLGSIAG7RRB42MNLQ224"].locals.aLocal).not.toBeDefined();
     });
 
     it("throws when account is not found", () => {
@@ -38,7 +39,7 @@ describe("app_local_del opcode", () => {
                 // No account.
             },            
             stack: [                
-                makeBytes(new Uint8Array(Buffer.from("AAAA"))),
+                makeBytes(addressToBytes("7JOPVEP3ABJUW5YZ5WFIONLPWTZ5MYX5HFK4K7JLGSIAG7RRB42MNLQ224")),
                 makeBytes(new Uint8Array(Buffer.from("aLocal"))),
                 makeBigInt(BigInt(6)),
             ],
