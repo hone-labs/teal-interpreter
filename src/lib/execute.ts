f26de2a203fdc018c077f63f5752949bc2699c72
Ashley Davis <ashley@codecapers.com.au>
2021-11-19 18:51:11 +1000

    Configuration can now be passed to the execution function.

--- src/lib/execute.ts
+++ src/lib/execute.ts
@@ -1,13 +1,13 @@
-import { TealInterpreter } from "..";
+import { ITealInterpreterConfig, TealInterpreter } from "..";
 import { IExecuteResult } from "./execute-result";
 
 //
 // Executes TEAL code and returns a result.
 //
-export function execute(tealCode: string): IExecuteResult {
+export function execute(tealCode: string, config?: ITealInterpreterConfig): IExecuteResult {
 
     const interpreter = new TealInterpreter();
-    interpreter.load(tealCode);
+    interpreter.load(tealCode, config);
 
     while (interpreter.step()) {
         // Step until done.
