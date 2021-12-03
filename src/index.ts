6baa67f76f792b89f0a49e12e36540746fb2667e
Ashley Davis <ashley@codecapers.com.au>
2021-12-03 14:04:11 +1000

    Now exporting isTypedValue for use in the teal debugger.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export { parse, IParseResult } from "./lib/parser";
 export { IOpcode } from "./lib/opcode";
 export { execute  } from "./lib/execute";
 export { IExecuteResult } from "./lib/execute-result";
-export { ValueType, IAccount, IExecutionContext } from "./lib/context";
+export { ValueType, IAccount, IExecutionContext, isTypedValue } from "./lib/context";
 export { ITealInterpreterConfig, ITable, ValueDef, IAccountDef,  } from "./lib/config";
 export { ITealInterpreter, TealInterpreter } from "./lib/interpreter";
