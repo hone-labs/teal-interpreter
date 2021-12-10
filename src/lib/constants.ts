9ee88facd54eff17ee021cd0402dd9bdeb9e7ed7
Ashley Davis <ashley@codecapers.com.au>
2021-12-10 11:30:25 +1000

    Added some name integer constants that were missing.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,10 +5,19 @@
 //
 
 export const constants: any = {
+    // https://developer.algorand.org/docs/get-details/dapps/avm/teal/specification/#oncomplete
     NoOp: 0,
     OptIn: 1,
     CloseOut: 2,
     ClearState: 3,
     UpdateApplication: 4,
     DeleteApplication: 5,
+
+    // https://developer.algorand.org/docs/get-details/dapps/avm/teal/specification/#typeenum-constants
+    pay: 0,
+    keyreg: 1,
+    acfg: 2,
+    axfer: 3,
+    afrz: 4,
+    appl: 5,
 };
