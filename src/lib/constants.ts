488ff6af872ae540b0c3d15a4a89c882cb18508f
Ashley Davis <ashley@codecapers.com.au>
2022-02-07 15:20:58 +1000

    Fixed broken constants.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,10 +14,10 @@ export const constants: any = {
     DeleteApplication: 5,
 
     // https://developer.algorand.org/docs/get-details/dapps/avm/teal/specification/#typeenum-constants
-    pay: 0,
-    keyreg: 1,
-    acfg: 2,
-    axfer: 3,
-    afrz: 4,
-    appl: 5,
+    pay: 1,
+    keyreg: 2,
+    acfg: 3,
+    axfer: 4,
+    afrz: 5,
+    appl: 6,
 };
