7b215a0addf897743bac2999c5b630d83004e36a
Ashley Davis <ashley@codecapers.com.au>
2022-02-28 15:07:23 +1000

    Updated exports.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 export { parse, IParseResult } from "./lib/parser";
 export { IOpcode } from "./lib/opcode";
 export { execute  } from "./lib/execute";
-export { ValueType, IAccount, IExecutionContext, isTypedValue } from "./lib/context";
+export { ITypedValue, ValueType, IAccount, IExecutionContext, isTypedValue } from "./lib/context";
 export { ITealInterpreterConfig, ITable, ValueDef, IAccountDef  } from "./lib/config";
 export { ITealInterpreter, TealInterpreter } from "./lib/interpreter";
 export { loadValue } from "./lib/convert";
