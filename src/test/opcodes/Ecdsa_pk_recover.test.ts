349402878e329018ddd574dc1a8b9740ce98b760
Ashley Davis <ashley@codecapers.com.au>
2022-03-04 13:25:43 +1000

    Fixed test name and details.

--- src/test/opcodes/Ecdsa_pk_recover.test.ts
+++ src/test/opcodes/Ecdsa_pk_recover.test.ts
@@ -3,7 +3,7 @@ import { opcodeDefs } from "../../lib/opcodes";
 import { Ecdsa_pk_recover } from "../../lib/opcodes/ecdsa_pk_recover";
 import { ec as EC } from "elliptic";
 
-describe("ecdsa_pk_decompress opcode", () => {
+describe("ecdsa_pk_recover opcode", () => {
 
     it ("can execute", () => {
 
@@ -12,7 +12,7 @@ describe("ecdsa_pk_decompress opcode", () => {
                 "0",
             ],
         };
-        const opcode = new Ecdsa_pk_recover(token, opcodeDefs.ecdsa_pk_decompress);
+        const opcode = new Ecdsa_pk_recover(token, opcodeDefs.ecdsa_pk_recover);
 
         const hash = new Uint8Array([0, 1, 2, 3, 4, 5]);
         const ec = new EC('secp256k1');
