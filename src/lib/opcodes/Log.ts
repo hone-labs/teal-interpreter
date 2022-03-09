a6564217538ba72c9656d9ee084603bffad09b6f
Ashley Davis <ashley@codecapers.com.au>
2022-03-09 13:33:00 +1000

    Fixed case issues that were failing the build pipeline.

--- src/lib/opcodes/Log.ts
+++
@@ -1,9 +0,0 @@
-import { Opcode } from "../opcode";
-import { IExecutionContext } from "../context";
-
-export class Log extends Opcode {
-
-    execute(context: IExecutionContext): void {
-        context.logState.push(this.popBytes(context));
-    }
-}
