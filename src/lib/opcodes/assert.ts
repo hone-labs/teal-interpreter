e2d6dae718decaf7866304b583de98d67dbab6b6
Ashley Davis <ashley@codecapers.com.au>
2022-03-14 15:20:29 +1000

    Added stack dump to the error message from the assert opcode.

--- src/lib/opcodes/assert.ts
+++ src/lib/opcodes/assert.ts
@@ -1,16 +1,17 @@
 import { Opcode } from "../opcode";
 import { IExecutionContext } from "../context";
+import { stringify } from "../utils";
 
 export class Assert extends Opcode {
 
     execute(context: IExecutionContext): void {
         const value = context.stack.pop()!;
         if (value.type !== "bigint") {
-            throw new Error(`assert expects one bigint on the stack, instead found ${value.type} (line: ${this.token.lineNo})`);
+            throw new Error(`assert expects one bigint on the stack, instead found ${value.type} (line: ${this.token.lineNo})\r\nStack:\r\n${stringify(context.stack)}`);
         }
 
         if (value.value as bigint === BigInt(0)) {
-            throw new Error(`assert expects a non-zero value on the stack (line: ${this.token.lineNo})`);
+            throw new Error(`assert expects a non-zero value on the stack (line: ${this.token.lineNo})\r\nStack:\r\n${stringify(context.stack)}`);
         }
     }
 }
