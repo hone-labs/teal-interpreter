ca50dd4674d74e0a6e46bef19ebe9d1c3b6402bf
Ashley Davis <ashley@codecapers.com.au>
2022-03-14 15:12:36 +1000

    Added line numbers to the error message from the assert opcode.

--- src/lib/opcodes/assert.ts
+++ src/lib/opcodes/assert.ts
@@ -6,11 +6,11 @@ export class Assert extends Opcode {
     execute(context: IExecutionContext): void {
         const value = context.stack.pop()!;
         if (value.type !== "bigint") {
-            throw new Error(`assert expectes one bigint on the stack, instead found ${value.type}`);
+            throw new Error(`assert expects one bigint on the stack, instead found ${value.type} (line: ${this.token.lineNo})`);
         }
 
         if (value.value as bigint === BigInt(0)) {
-            throw new Error(`assert expects a non-zero value on the stack`);
+            throw new Error(`assert expects a non-zero value on the stack (line: ${this.token.lineNo})`);
         }
     }
 }
